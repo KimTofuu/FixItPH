375e083aa05e5bae34c9afd2b59eb795e6e847ef
KimTofuu <152471516+KimTofuu@users.noreply.github.com>
2025-09-04 17:39:05 +0800

    logins done with token polishing pa

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../models/Users');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 // Registration route
 router.post('/register', async (req, res) => {
   try {
-    const { email, password, confirmPassword } = req.body;
-    
+    const { fName, lName, email, password, confirmPassword, barangay, municipality } = req.body;
+
     if (password !== confirmPassword) {
       return res.status(400).json({ message: 'Passwords do not match' });
     }
@@ -22,7 +24,7 @@ router.post('/register', async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     // Create new user
-    const newUser = new User({ email, password: hashedPassword });
+    const newUser = new User({ fName, lName, email, password: hashedPassword , barangay, municipality });
     await newUser.save();
 
     res.status(201).json({ message: 'User registered successfully' });
@@ -30,6 +32,41 @@ router.post('/register', async (req, res) => {
     res.status(500).json({ message: 'Server error' });
     console.error(err);
   }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    // Compare password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    // Set token in cookie
+    res.cookie('token', token, { httpOnly: true, secure: true, sameSite: 'strict' });
+
+    res.status(200).json({ message: 'Login successful', token });
+    console.log(token);
+  } catch (err) {
+    res.status(500).json({ message: 'Server error' });
+    console.error(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  res.clearCookie('token');
+  res.status(200).json({ message: 'Logout successful' });
 });
 
 module.exports = router;
