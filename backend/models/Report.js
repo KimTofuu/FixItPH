4d17b344d0074ecd2789642d0b98f2a19f88a144
KimTofuu <152471516+KimTofuu@users.noreply.github.com>
2025-09-07 22:39:03 +0800

    nag add lang ng index sa models

--- backend/models/Report.js
+++ backend/models/Report.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const ReportSchema = new mongoose.Schema({
-    image: { type: String, required: true },
-    description: { type: String, required: true },
-    location: { type: String, required: true },
-    issueType: { type: String, required: true },
-    status: { type: String, enum: ['pending', 'in-progress', 'resolved'], default: 'pending' },
+    image: { type: String, required: true, index: true },
+    description: { type: String, required: true, index: true },
+    location: { type: String, required: true, index: true },
+    issueType: { type: String, required: true, index: true },
+    status: { type: String, enum: ['pending', 'in-progress', 'resolved'], default: 'pending', index: true },
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 }, { collection: 'reports' });
 
